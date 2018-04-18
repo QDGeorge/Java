b85cc59ca1b6498d3cde032f7d489c6743a5bdf5
George123321 <gosha-dem@ya.ru>
2018-04-19 00:37:30 +0300

    Жизнь живет

--- MIPT_SEMINARS/sem_9/src/SimpleGUI.java
+++ MIPT_SEMINARS/sem_9/src/SimpleGUI.java
@@ -1,42 +1,87 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.geom.Line2D;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+public class SimpleGUI extends JFrame { // JFrame - класс, представляющий собой окно с рамкой и с кнопками: "свернуть", "во весь экран", "закрыть"
+    private Color color; // Объявили переменную типа Color, она отвечает за цвета
+    private List<Point> fillCells; // Объявили список, содержащий клетки типа Point
 
-public class SimpleGUI extends JFrame {
-    private Color color;
+    public SimpleGUI() { //Конструктор без параметров, который создает список с клетками и рисует кнопки
+        Game usefull_game = new Game();
+        fillCells = new ArrayList(usefull_game.world_size); // Создаем список с клетками, начальный размер списка = 10,
+        setSize((usefull_game.world_size + 2) * 10, (usefull_game.world_size + 8) * 10); // устанавливаем размер окна
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // позволяет указать действие, которое необходимо выполнить, когда закрываем окно, нажатием на крестик (в данном случае мы закрываем окно)
 
-    public SimpleGUI() {
-        setSize(450, 450);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        color = Color.BLACK;    // "Включаем" чёрный цвет для наших "рисунков"
+        JPanel panel = new JPanel(); // Создаём панель (прямоугольное пространство, на котором можно размещать элементы)
+        getContentPane().add(panel); // Добавляем панель на экран
 
-        color = Color.RED;
-        JPanel panel = new JPanel();
-        getContentPane().add(panel);
-
-        JButton button1 = new JButton("Start!");
-        JButton button2 = new JButton("Stop!");
-        JButton button3 = new JButton("Reset!");
+        JButton button = new JButton("Start"); // создаём кнопки с надписями и размещаем их на панели
+        panel.add(button);
+        JButton button1 = new JButton("Stop");
         panel.add(button1);
+        JButton button2 = new JButton("Reset");
         panel.add(button2);
-        panel.add(button3);
-        /*
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                color = Color.BLUE;
-                SimpleGUI.this.repaint();
-            }
-        });*/
     }
 
-    public static synchronized void main(String []args) throws Exception {
+    public void paint(Graphics g) { // Функция, которая рисует клетки
+        Game usefull_game = new Game();
+        super.paint(g); //
+        Graphics2D g2 = (Graphics2D) g; // создали график на плоскости
+        g2.setColor(Color.BLACK); // выбрали цвет для нашего графика
+        /*
+        for (int i = 10; i <= usefull_game.world_size*10; i = i + 10) { // Рисуем вертикальные линии x: от 10 позиции слева до 500; y: от 70 позиции сверху до 510 (их размер как-то там автоматически определен)
+            Line2D lin = new Line2D.Float(i, 70, i, (usefull_game.world_size + 7) * 10);
+            g2.draw(lin);
+        }
+        for (int i = 70; i <= (usefull_game.world_size + 6) * 10; i = i + 10) { // Рисуем горизонтальные линии
+            Line2D lin = new Line2D.Float(10, i, (usefull_game.world_size + 1) * 10, i);
+            g2.draw(lin);
+        }
+        g.setColor(Color.BLACK);
+        g.drawRect(10,70,(usefull_game.world_size) * 10,(usefull_game.world_size) * 10); // рисуем прямоугольник в соответствии с расположением линий*/
+
+        for (Point fillCell : fillCells) { // проходимся циклом по списку из клеток
+            int cellX = 10 + (fillCell.x * 10); // координата x для нашей выбранной из списка клетки
+            int cellY = 70 + (fillCell.y * 10); // координата y
+            g.setColor(Color.GREEN);    // выбираем зелёный цвет для дальнейшего рисунка
+            g.fillRect(cellX, cellY, 10, 10); // закрашиваем клетку
+        }
+    }
+
+    public void fillCell(int x, int y) {
+        fillCells.add(new Point(x, y)); // добавляем список клеток на график
+        repaint();
+    }
+
+    public void rePaint(Game game) {
+        // сначала очистим список
+        if (fillCells.size() != 0) {
+            for (int i = 0; i < fillCells.size(); i++) {
+                fillCells.remove(i);
+            }
+        }
+        for (int i = 0; i < game.world_size; i++) {
+            for (int j = 0; j < game.world_size; j++) {
+                if (game.world[i][j].is_live) {
+                    this.fillCell(i, j);
+                    repaint();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
         SimpleGUI s = new SimpleGUI();
+        s.fillCell(7, 7); // размещаем клетку в точке (7,7)
+        s.fillCell(49, 49); // размещаем клетку в точке (30,30)
         s.setVisible(true);
     }
 }
--- MIPT_SEMINARS/sem_9/src/game.java
+++ MIPT_SEMINARS/sem_9/src/game.java
@@ -1,23 +1,25 @@
+import javax.swing.*;
+import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class game {
-    public int world_height = 20;
-    public int world_weight = 20;
-    public point world[][] = new point[world_height][world_weight]; // TODO: надо сделать world полем класа game
+public class Game {
+    public int world_size = 100;
+    public point world[][] = new point[world_size][world_size];
 
     public void init_field() {
-        for (int i = 0; i < world_height; i++) {
-            for (int j = 0; j < world_weight; j++) {
+        for (int i = 0; i < world_size; i++) {
+            for (int j = 0; j < world_size; j++) {
                 world[i][j] = new point();
+                if (Math.random() < 0.5) {
+                    world[i][j].is_live = false;
+                }
+                else {
+                    world[i][j].is_live = true;
+                }
             }
         }
-        world[0][1].is_live = true;
-        world[1][2].is_live = true;
-        world[2][0].is_live = true;
-        world[2][1].is_live = true;
-        world[2][2].is_live = true;
     }
 
     public int count_number_of_neighbours(int l, int s) {
@@ -26,8 +28,8 @@ public class game {
         int horizontal[] = {-1, 0, 1, 1, 1, 0, -1, -1};
 
         for (int type = 0; type < 8; type++) {
-            if ((0 <= l + vertical[type]) && (l + vertical[type] < world_height) &&
-                    (0 <= s + horizontal[type]) && (s + horizontal[type] < world_weight) &&
+            if ((0 <= l + vertical[type]) && (l + vertical[type] < world_size) &&
+                    (0 <= s + horizontal[type]) && (s + horizontal[type] < world_size) &&
                     (world[l + vertical[type]][s + horizontal[type]].is_live)) {
                 number_of_neighbours++;
             }
@@ -36,22 +38,22 @@ public class game {
     }
 
     public void refield() {
-        point new_world[][] = new point[world_height][world_weight];
+        point new_world[][] = new point[world_size][world_size];
 
-        for (int i = 0; i < world_height; i++) {
-            for (int j = 0; j < world_weight; j++) {
+        for (int i = 0; i < world_size; i++) {
+            for (int j = 0; j < world_size; j++) {
                 new_world[i][j] = new point();
             }
         }
 
-        for (int i = 0; i < world_height; i++) {
-            for (int j = 0; j < world_weight; j++) {
+        for (int i = 0; i < world_size; i++) {
+            for (int j = 0; j < world_size; j++) {
                 new_world[i][j].is_live = world[i][j].is_live;
             }
         }
 
-        for (int i = 0; i < world_height; i++) {
-            for (int j = 0; j < world_weight; j++) {
+        for (int i = 0; i < world_size; i++) {
+            for (int j = 0; j < world_size; j++) {
                 if (!world[i][j].is_live && count_number_of_neighbours(i, j) == 3) {
                     new_world[i][j].is_live = true;
                 }
@@ -64,42 +66,42 @@ public class game {
             }
         }
 
-        for (int i = 0; i < world_height; i++) {
-            for (int j = 0; j < world_weight; j++) {
+        for (int i = 0; i < world_size; i++) {
+            for (int j = 0; j < world_size; j++) {
                 world[i][j].is_live = new_world[i][j].is_live;
             }
         }
     }
 
-    public static synchronized void main(String[] args) throws Exception {
-        game g = new game();
+    public static void startGame(SimpleGUI s) {
+        Game g = new Game();
         g.init_field();
-
-        for (int i = 0; i < g.world_height; i++) {
-            for (int j = 0; j < g.world_weight; j++) {
+        /*
+        for (int i = 0; i < g.world_size; i++) {
+            for (int j = 0; j < g.world_size; j++) {
                 System.out.print(" " + ((g.world[i][j].is_live) ? 1 : 0));
             }
             System.out.println("");
         }
-        System.out.println("");
+        System.out.println("");*/
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
         Runnable task = new Runnable() {
             @Override
             public void run() {
                 g.refield();
-                for (int i = 0; i < g.world_height; i++) {
-                    for (int j = 0; j < g.world_weight; j++) {
-                        System.out.print(" " + ((g.world[i][j].is_live) ? 1 : 0));
-                    }
-                    System.out.println("");
-                }
-                System.out.println("");
+                s.rePaint(g);
             }
         };
-
         int initialDelay = 0;
-        int period = 1;
+        int period = 2;
         executor.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.SECONDS);
     }
+
+    public static synchronized void main(String[] args) {
+        SimpleGUI s = new SimpleGUI();
+        startGame(s);
+        s.setVisible(true);
+    }
 }
+
